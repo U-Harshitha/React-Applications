05a89c6077c47e632bd2c80b80581b7b077f6e98
U Harshitha <122729066+U-Harshitha@users.noreply.github.com>
2024-06-04 23:11:49 +0530

    Final Fixes

--- MemoriesPage/server/routes/posts.js
+++ MemoriesPage/server/routes/posts.js
@@ -5,10 +5,10 @@ import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from 
 const router = express.Router();
 
 router.get('/', getPosts);
-//router.post('/', createPost);
-//router.get('/:id', getPost);
-//router.patch('/:id', updatePost);
-//router.delete('/:id', deletePost);
-//router.patch('/:id/likePost', likePost);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
