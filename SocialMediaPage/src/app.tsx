42ffc8bc463bc19cc5e6fb2ced9afb83280c4b26
U Harshitha <122729066+U-Harshitha@users.noreply.github.com>
2024-06-10 08:00:10 +0530

    Social Media page files

--- SocialMediaPage/src/app.tsx
+++ SocialMediaPage/src/app.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import './globals.css'
+import { Routes, Route } from "react-router-dom";
+import SignupForm from "./_auth/forms/SignupForm";
+import SigninForm from "./_auth/forms/SigninForm";
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
 const App = () => {
   return (
-    <div>App!!</div>
+    <main className='flex h-screen'>
+        <h1>hi</h1>
+        <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home />} />
+        </Route>
+        </Routes>
+    </main>
   )
 }
 
